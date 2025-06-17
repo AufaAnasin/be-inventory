dba7d2af64f7a2b00ec70b9dad343fc179b7c921
HelloCaptainAufa737 <anasinaufa@gmail.com>
2025-06-17 16:43:37 +0700

    [done] create user and product model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -10,6 +12,19 @@ app.use(express.json());
 sequelize.authenticate()
   .then(() => console.log('Database connection has been established successfully.'))
   .catch(err => console.error('Unable to connect to the database:', err));
+
+// sequelize.sync({ force: true })
+//   .then(() => console.log('Database synchronized'))
+//   .catch(err => console.error('Sync error:', err));
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const [results] = await sequelize.query('SELECT 1 + 1 AS result');
+    res.json({ result: results[0].result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT} at ${new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' })}`));
